0b8c40e98f3678d79e65e8ad559504d4080d0cb6
ammbalestro@gmail.com <artur.balestro@objectedge.com>
2020-08-20 23:13:54 -0300

    lesson 64

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import { Text, StyleSheet, View, TextInput } from "react-native";
 
 const TextScreen = () => {
+    const [name, setName] = useState('');
     return (
         <View>
-            <TextInput style={styles.input} autoCapitalize="none" autoCorrect={false} />
+            <Text>Enter the name</Text>
+            <TextInput style={styles.input} autoCapitalize="none" autoCorrect={false} value={name} onChangeText={(newValue) => setName(newValue)} />
+            <Text>My name is {name}</Text>
         </View>
     );
 }
