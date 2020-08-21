cbb8378dc9460a25ab995d6d2d5fe73da45d8815
ammbalestro@gmail.com <artur.balestro@objectedge.com>
2020-08-20 22:55:24 -0300

    lesson 62

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,25 +1,19 @@
 import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, TextInput } from "react-native";
 
 const TextScreen = () => {
     return (
         <View>
-            <Text>Text Screen</Text>
+            <TextInput style={styles.input} autoCapitalize="none" autoCorrect={false} />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    textStyle: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        borderWidth: 1,
-        fontWeight: 'bold',
-        marginVertical: 50,
-        padding: 20,
-        textAlign: 'center',
-        width: '100%'
+    input: {
+        margin: 15,
+        borderColor: 'black',
+        borderWidth: 1
     }
 });
 
