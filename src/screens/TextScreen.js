ce562d86cb99657dadeaa41503e4b12eb59f882c
ammbalestro@gmail.com <artur.balestro@objectedge.com>
2020-08-20 23:24:36 -0300

    lesson 64

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -3,11 +3,19 @@ import { Text, StyleSheet, View, TextInput } from "react-native";
 
 const TextScreen = () => {
     const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
     return (
         <View>
             <Text>Enter the name</Text>
             <TextInput style={styles.input} autoCapitalize="none" autoCorrect={false} value={name} onChangeText={(newValue) => setName(newValue)} />
             <Text>My name is {name}</Text>
+            {name === 'Artur' ? <Text>Oh my god! You have the same name as me!</Text> : null}
+
+            <View>
+            <Text>Enter the password</Text>
+            <TextInput style={styles.input} autoCapitalize="none" autoCorrect={false} value={password} onChangeText={(newValue) => setPassword(newValue)} />
+            {password.length < 4 ? <Text>Password has less than 4 characters</Text> : null}
+            </View>
         </View>
     );
 }
